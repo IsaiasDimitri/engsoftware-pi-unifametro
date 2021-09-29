17715b042d50eae2a8828432331ba3728bfc2a2f
IsaiasDimitri <isaias.dimitri@hp.com>
2021-09-29 20:09:07 -0300

    Changing default DB to Postgres and improving docker-compose configs

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,28 @@
-FROM python:3
-ENV PYTHONUNBUFFERED=1
+FROM python:3.8.6-slim-buster
+
+ARG DJANGO_ENV
+
+ENV DJANGO_ENV=${DJANGO_ENV} \
+    # python
+    PYTHONUNBUFFERED=1 \
+    PYTHONDONTWRITEBYTECODE=1 \
+    PYTHONHASHSEED=random
+
+# system deps
+RUN apt-get update \
+    && apt-get install --no-install-recommends -y \
+    # deps for installing poetry
+    curl \
+    # deps for building python deps
+    build-essential \
+    # cleaning cache
+    && apt-get autoremove -y && apt-get clean -y && rm -rf /var/lib/apt/lists/*
+
 WORKDIR /api
 COPY requirements.txt /api/
 RUN pip install -r requirements.txt
 COPY . /api/
+COPY ./docker-entrypoint.sh /docker-entrypoint.sh
+RUN chmod +x '/docker-entrypoint.sh'
+
+CMD ["/docker-entrypoint.sh"]
--- main_project/api/models.py
+++ main_project/api/models.py
@@ -3,24 +3,7 @@ from django.db import models
 # Create your models here.
 
 # class Conteudo(models.Model):
-#     created = models.DateTimeField(auto_now_add=True)
-#     title = models.CharField(max_length=100, blank=True, default='')
-
 
 # class Comentario(models.Model):
-#     created = models.DateTimeField(auto_now_add=True)
-#     popularity = models.IntegerField(default=0)
-#     content = models.OneToOneField(
-#         Conteudo,
-#         on_delete=models.CASCADE
-#     )
 
 # class Postagem(models.Model):
-#     created = models.DateTimeField(auto_now_add=True)
-#     popularity = models.IntegerField(default=0)
-#     content = models.OneToOneField(
-#         Conteudo,
-#         on_delete=models.CASCADE
-#     )
-#     class Meta:
-#         ordering = ['created']
